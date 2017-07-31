fb7576d3cf64bf51285b3ec18064aaf9a7fd0c32
jono3028 <jonowen@hotmail.com>
2017-07-31 09:40:28 -0700

    File structrure correction

---
+++ README.md
@@ -0,0 +1,6 @@
+### Alexa Skill: Alive or Dead
+
+## Intro:
+
+This skill was created during the Alexa Dev Day held at Amazon HQ on July 19, 2017. A fact based skill provides a response based upon a user query. This skill parses data from the website www.deadoraliveinfo.com and returns whether a celebrity is alive or dead along with additional information like their age, or a breif bio. The website deadoraliveinfo does not offer an API, data was scraped following the generation of a URL from the user defined input.
+
--- src/index.js
+++ src/index.js
@@ -13,24 +13,6 @@ var languageStrings = {
     }
     // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
 };
-var data = {
-  'names' : [
-    { 'name' : 'Adam West',
-      'field' : 'Entertainment',
-      'info' : 'He played Batman in the 1960s TV series "Batman"',
-      'birth' : new Date(1928, 9, 19),
-      'death' : new Date(2017, 6, 9),
-      'cause' : 'cancer'
-    },
-    { 'name' : 'Sammy Hagar',
-      'field' : 'Music',
-      'info' : 'Rock singer, solo hits include "Your Love Is Driving Me Crazy" and "I Can\'t Drive 55", later became lead singer of Van Halen, heard on their hits "Why Can\'t This Be Love" and "When It\'s Love", among other songs',
-      'birth' : new Date(1947, 10, 13),
-      'death' : null,
-      'cause' : null
-    },
-  ]  
-}
 
 // 2. Skill Code =======================================================================================================
 
@@ -38,9 +20,9 @@ var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-
+    
     // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
-    ///alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+    // alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -53,7 +35,7 @@ var handlers = {
     },
 
     'Unhandled': function() {
-        this.emit(':tell', 'Who are you asking about?');
+        this.emit(':tell', 'who would you like to know about?');
     },
 
     'AboutIntent': function () {
@@ -64,32 +46,26 @@ var handlers = {
       var say = ''
 
       var name = this.event.request.intent.slots.name.value
-      var celeb = getCelebByName(name)
-      var age = getAge(celeb.birth, celeb.death)
+      
+      var celeb = requestData(name, celeb => {
+        var age = celeb.age()
 
-    	if (celeb.death) {
-    	    say = "Sorry, " + celeb.name + " has died."
-    	} else {
-    	    say = celeb.name + " is still alive and is " + age + " years old."
-    	}
-        this.emit(":ask",say);
+        if (celeb.death) {
+            say = "sorry, " + celeb.name + " has died. would you like to know more?"
+        } else {
+            say = celeb.name + " is still alive and is " + age + " years old. would you like to know more?"
+        }
+          this.emit(":ask", say);
+      })
     },
 
     'AMAZON.YesIntent': function () {
         var restaurantName = this.attributes['restaurant'];
         var restaurantDetails = getRestaurantByName(restaurantName);
 
-        var say = restaurantDetails.name
-            + ' is located at ' + restaurantDetails.address
-            + ', the phone number is ' + restaurantDetails.phone
-            + ', and the description is, ' + restaurantDetails.description
-            + '  I have sent these details to the Alexa App on your phone.  Enjoy your meal! <say-as interpret-as="interjection">bon appetit</say-as>' ;
+        var say = "known for there work in " + celeb.field + ". " + celeb.name + celeb.info;
 
-        var card = restaurantDetails.name + '\n' + restaurantDetails.address + '\n'
-            + data.city + ', ' + data.state + ' ' + data.postcode
-            + '\nphone: ' + restaurantDetails.phone + '\n';
-
-        this.emit(':tellWithCard', say, restaurantDetails.name, card);
+        this.emit(':tell', say);
 
     },
 
@@ -111,75 +87,95 @@ var handlers = {
 //    END of Intent Handlers {} ========================================================================================
 // 3. Helper Function  =================================================================================================
 
-function getCelebByName (name) {
-    var celeb
-    
-    for (var i = 0; i < data.names.length; i++) {
-      var libName = data.names[i].name
-        if (libName.toLowerCase() == name.toLowerCase()) {
-            celeb = data.names[i]
-        }
-    }
-    
-    return celeb;
-}
-
-function getAge(birth, death) {
-    var birthDate = new Date(birth)
-    var endDate = (death) ? new Date(death) : new Date()
-    var age = endDate.getFullYear() - birthDate.getFullYear()
-    var m = endDate.getMonth() - birthDate.getMonth()
-
-    if (m < 0 || (m === 0 && endDate.getDate() < birthDate.getDate())) {
-        age--
-    }
-    return age;
-}
-
-var http = require('http')
-var querystring = require('querystring')
-var postData = querystring.stringify({
-  'displayname': 'Adam West'
-})
+var http = require('http');
 
 class Celeb {
   
   constructor(data) { // param is a dict. all values are in string format, DOB & DOD are DD/MM/YYYY 
-    this.birth = this.dateConst(data.DOB)
-    this.death = this.dateConst(data.DOD)
-    this.field = data.field
-    this.info = data.info
-    this.cause = this.causeConst(data.cause)
+    this.name;
+    this.birth = this.dateConst(data.DOB);
+    this.death = this.dateConst(data.DOD);
+    this.field = data.field;
+    this.info = data.info;
+    this.cause = this.causeConst(data.cause);
   }
 
   dateConst(day) {
     
     let _day = day.split('/'),
-      _date = new Date(_day[2],_day[0],_day[1])
+      _date = new Date(_day[2],_day[0],_day[1]);
     
     if (_date == 'Invalid Date'){return null}
     
-    return _date
+    return _date;
   }
 
   causeConst(cause) {
     
     if (cause.length > 20) {return 'Unknown'}
     
-    return cause
+    return cause;
   }
+
+  age() {
+    var endDate = (this.death) ? this.death : new Date();
+    var age = endDate.getFullYear() - this.birth.getFullYear();
+    var m = endDate.getMonth() - this.birth.getMonth();
+
+    if (m < 0 || (m === 0 && endDate.getDate() < this.birth.getDate())) {
+        age--;
+    }
+
+    return age;
+  }
+}
+
+function requestData(celebName, callback) {
+  var options = {
+    name: celebName,
+    hostname: 'www.deadoraliveinfo.com',
+    port: 80,
+    path: pathGen(celebName),
+    method: 'GET'
+  }
+
+  var req = http.request(options, (res) => {
+    res.setEncoding('utf8');
+    
+    var rawData = '';
+    
+    res.on('data', (chunk) => {
+      rawData += chunk
+    });
+    
+    res.on('end', () => {
+      try {
+        let celeb =  new Celeb(dataParse(rawData))
+        celeb.name = celebName
+        callback(celeb)
+      } catch (e) {
+        console.error(`ERROR: ${e.message}`)
+      }
+    })
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`)
+  });
+
+  req.end();
 }
 
 function pathGen (nameStr) {  // function used to take the celeb's name and generate the path for http GET command
   
-  var splitName = nameStr.split(' ')
+  var splitName = nameStr.split(' ');
 
   if (splitName.length > 2) {
     splitName[0] = splitName[0] + '+' + splitName[1]
     splitName[1] = splitName[2]
   }
 
-  return `/dead.nsf/${splitName[1].charAt(0).toLowerCase()}names-nf/${splitName[1] + '+' + splitName[0]}`
+  return `/dead.nsf/${splitName[1].charAt(0).toLowerCase()}names-nf/${splitName[1] + '+' + splitName[0]}`;
 }
 
 function dataParse (rawData) { // Parse the desired data from the downloaded data
@@ -192,48 +188,26 @@ function dataParse (rawData) { // Parse the desired data from the downloaded dat
     offset = (idxDeath == 38)? 63 : 39,                         // if celeb is alive idxDeath == 38, offset = 63 if alive, 39 if dead
     idxBirth = rawData.indexOf('Date of Birth:') + offset,      // find start of date string
     idxCause = rawData.indexOf('Cause of Death', idxDeath) + 46, // find start of cause string
-    idxCauseEnd = rawData.indexOf('</td>', idxCause) 
-               // find end of cause string
+    idxCauseEnd = rawData.indexOf('</td>', idxCause);            // find end of cause string
+
   return {
     field: rawData.slice(idxField, idxFieldEnd),
     info: rawData.slice(idxInfo,idxInfoEnd),
     cause: rawData.slice(idxCause,idxCauseEnd),
     DOB: rawData.slice(idxBirth, idxBirth + 10),
     DOD: rawData.slice(idxDeath, idxDeath + 10)
-  }
+  };
 }
 
-const options = {
-  name: 'Adam West',
-  hostname: 'www.deadoraliveinfo.com',
-  port: 80,
-  path: pathGen('Jimmy Carter'),
-  method: 'GET'
-};
+function getCelebByName (name) {
+  var celeb;
+  
+  for (var i = 0; i < data.names.length; i++) {
+    var libName = data.names[i].name;
+      if (libName.toLowerCase() == name.toLowerCase()) {
+          celeb = data.names[i];
+      }
+  }
 
-console.log('PATH: ', options.path)
-
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`)
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
-  res.setEncoding('utf8');
-  let rawData = ''
-  res.on('data', (chunk) => {
-    rawData += chunk
-  });
-  res.on('end', () => {
-    try {
-      var celeb = new Celeb(dataParse(rawData))
-      console.log('CELEB', celeb)
-    } catch (e) {
-      console.error(`ERROR: ${e.message}`)
-    }
-  })
-});
-
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-req.write(postData)
-req.end();
+  return celeb;
+}
